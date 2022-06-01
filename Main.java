536fbc8e8be7a94782a162b1cbd2e29e3c43969c
MegaDen58 <e@w.com>
2022-06-01 12:40:24 +0300

    66

--- Main.java
+++ Main.java
@@ -1,40 +1,76 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
 
-class Formula{
-    private static Formula formula = new Formula();
-    private double x, y;
-    private Formula(){
+interface Observable {
+    void notifyObservers();
+    void regObserver(Observer obs);
+}
 
+interface Observer{
+    void update(String news);
+}
+
+class Group implements Observable{
+    List<Observer> list = new ArrayList<>();
+    String info = "Новость";
+    void setInfo(String info){
+        this.info = info;
+        notifyObservers();
     }
-    public static Formula getInstance(){
-        return formula;
+
+    @Override
+    public void notifyObservers() {
+        for(Observer obs: list){
+            obs.update(info);
+        }
     }
-    public void setX(double x){
-        this.x = x;
+
+    @Override
+    public void regObserver(Observer o) {
+        list.add(o);
     }
-    public void setY(double y){
-        this.y = y;
+}
+class User implements Observer{
+    String name;
+    public User(String name){
+        this.name = name;
     }
-    public double Decision(){
-        return (2 * x) + (3 / y);
-    }
-    public double getX(){
-        return x;
-    }
-    public double getY(){
-        return y;
+
+    @Override
+    public void update(String info) {
+        System.out.println(name + " получил новость от сообщества "+ info + "\n======================");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Formula formula = Formula.getInstance();
-        formula.setX(5);
-        formula.setY(2);
-        System.out.println(formula.Decision());
+        Group csgo = new Group();
+        Group memes = new Group();
+        Group sport = new Group();
+        Group news = new Group();
+        User vasya = new User("Вася");
+        User dima = new User("Дима");
+        // Подписываем Васю на 3 группы
+        csgo.regObserver(vasya);
+        memes.regObserver(vasya);
+        news.regObserver(vasya);
+        // Подписываем Диму на 4 группы
+        csgo.regObserver(dima);
+        news.regObserver(dima);
+        sport.regObserver(dima);
+        memes.regObserver(dima);
 
-        Formula formula1 = Formula.getInstance();
-        System.out.println(formula1.getX());
-        System.out.println(formula1.getY());
+        csgo.setInfo("CSGO\nБумыча кикнули из нави :(");
+        memes.setInfo("MEMES\nШтирлиц пришел на встречу со связным в знакомый бар и заказал\n" +
+                "100 грамм водки.\n" +
+                "- Водка у нас кончилась еще два дня назад, - извинился бармен.\n" +
+                "- Ну, тогда 100 грамм коньячку.\n" +
+                "- Коньячок у нас кончился вчера, - огорченно сказал бармен.\n" +
+                "- Ну, а пиво-то есть? - спросил Штирлиц.\n" +
+                "- Увы, закончилось сегодня утром, - сказал бармен.\n" +
+                "\"Значит, связной уже здесь\", - подумал Штирлиц...\n");
+        news.setInfo("NEWS\nКолобок повесился");
+        sport.setInfo("SPORT\nСпорт - жестокая игра");
     }
 }
